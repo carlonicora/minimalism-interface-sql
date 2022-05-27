46667dc0d764a3c06cf738ebf60d1d2c8f47fc53
Sergey Kuzminich <KuzminichSergey@yandex.ru>
2022-05-27 12:09:44 +0300

    QueryFactory support in crate update delete

--- src/Abstracts/AbstractSqlIO.php
+++ src/Abstracts/AbstractSqlIO.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace CarloNicora\Minimalism\Interfaces\Sql\Abstracts;
 
 use CarloNicora\Minimalism\Enums\HttpCode;
@@ -9,6 +10,8 @@ use CarloNicora\Minimalism\Interfaces\Cache\Interfaces\CacheInterface;
 use CarloNicora\Minimalism\Interfaces\SimpleObjectInterface;
 use CarloNicora\Minimalism\Interfaces\Sql\Interfaces\SqlDataObjectInterface;
 use CarloNicora\Minimalism\Interfaces\Sql\Interfaces\SqlInterface;
+use CarloNicora\Minimalism\Interfaces\Sql\Interfaces\SqlQueryFactoryInterface;
+use Exception;
 
 class AbstractSqlIO implements SimpleObjectInterface
 {
@@ -18,8 +21,8 @@ class AbstractSqlIO implements SimpleObjectInterface
      * @param CacheInterface|null $cache
      */
     public function __construct(
-        protected ObjectFactory $objectFactory,
-        protected SqlInterface $data,
+        protected ObjectFactory   $objectFactory,
+        protected SqlInterface    $data,
         protected ?CacheInterface $cache,
     )
     {
@@ -32,11 +35,11 @@ class AbstractSqlIO implements SimpleObjectInterface
      * @throws MinimalismException
      */
     protected function returnSingleValue(
-        array $recordset,
-        ?string $recordType=null,
+        array   $recordset,
+        ?string $recordType = null,
     ): array
     {
-        if ($recordset === [] || $recordset === [[]]){
+        if ($recordset === [] || $recordset === [[]]) {
             throw new MinimalismException(
                 status: HttpCode::NotFound,
                 message: $recordType === null
@@ -49,13 +52,15 @@ class AbstractSqlIO implements SimpleObjectInterface
     }
 
     /**
-     * @param SqlDataObjectInterface $dataObject
+     * @template InstanceOfType
+     * @param SqlDataObjectInterface<InstanceOfType>|SqlQueryFactoryInterface|array $dataObject
      * @param CacheBuilderInterface|null $cache
-     * @return SqlDataObjectInterface
+     * @return InstanceOfType
+     * @throws Exception
      */
     public function create(
-        SqlDataObjectInterface $dataObject,
-        ?CacheBuilderInterface $cache = null
+        SqlDataObjectInterface|SqlQueryFactoryInterface|array $dataObject,
+        ?CacheBuilderInterface                                $cache = null
     ): SqlDataObjectInterface
     {
         $result = $this->data->create(
@@ -70,13 +75,13 @@ class AbstractSqlIO implements SimpleObjectInterface
     }
 
     /**
-     * @param SqlDataObjectInterface $dataObject
+     * @param SqlDataObjectInterface|SqlQueryFactoryInterface|array $dataObject
      * @param CacheBuilderInterface|null $cache
      * @return void
      */
     public function update(
-        SqlDataObjectInterface $dataObject,
-        ?CacheBuilderInterface $cache = null
+        SqlDataObjectInterface|SqlQueryFactoryInterface|array $dataObject,
+        ?CacheBuilderInterface                                $cache = null
     ): void
     {
         $this->data->update(
@@ -89,17 +94,17 @@ class AbstractSqlIO implements SimpleObjectInterface
     }
 
     /**
-     * @param SqlDataObjectInterface $dataObject
+     * @param SqlDataObjectInterface|SqlQueryFactoryInterface|array $dataObjectOrQueryFactory
      * @param CacheBuilderInterface|null $cache
      * @return void
      */
     public function delete(
-        SqlDataObjectInterface $dataObject,
-        ?CacheBuilderInterface $cache = null
+        SqlDataObjectInterface|SqlQueryFactoryInterface|array $dataObjectOrQueryFactory,
+        ?CacheBuilderInterface                                $cache = null
     ): void
     {
         $this->data->delete(
-            queryFactory: $dataObject,
+            queryFactory: $dataObjectOrQueryFactory,
         );
 
         if ($cache !== null) {
